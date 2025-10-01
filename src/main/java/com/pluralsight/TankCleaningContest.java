93ffd73c0e2ae1684ca0e76596980ce9ccc5c069
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-30 21:36:55 -0500

    split names and times & compare and print commit

--- src/main/java/com/pluralsight/TankCleaningContest.java
+++ src/main/java/com/pluralsight/TankCleaningContest.java
@@ -21,8 +21,29 @@ public class TankCleaningContest {
         // now it's time to separate the string
         // Split the cleaning data into two parts: names and minutes
 
-        String[] names = cleaningData.
+        String[] parts = cleaningData.split("\\|"); // use .split("\\|"); OR .split(Pattern.quote("|"));
+        // parts[0] = names
+        // parts[1] = minutes
 
+        // Split the names
+        String[] names = parts[0].split(":");
+        String name1 = names[0]; // Maggie
+        String name2 = names[1]; // Paola
+
+        // Split the times
+        String[] minutes = parts[1].split(":");
+        int name1Time = Integer.parseInt(minutes[0]); // Maggie spent 35 minutes cleaning
+        int name2Time = Integer.parseInt(minutes[1]); // Paola spent 50 minutes cleaning
+
+        // Compare the times and Print the winner
+        if (name1Time > name2Time) {
+            System.out.println("The Winner is " + name1);
+        } else if (name2Time > name1Time) {
+            System.out.println("The Winner is " + name2);
+        } else {
+            System.out.println("It's a Tie! " + name1 + " and " + name2 + " are both winners!");
+        }
+        scanner.close(); // closing the scanner to make sure code is clean and finished scanning
 
     }
 }
